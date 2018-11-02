021f7dde02045a34440733ce87dac07f56cca07c
Falko Benthin <f.benthin@jpberlin.de>
2018-11-02 21:58:00 +0100

    mit projektdaten

---
+++ build/precache-manifest.c216071f33de543568419b6061357be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21c9402aaa92a3c24cab",
+    "url": "/static/css/main.6640bdfb.chunk.css"
+  },
+  {
+    "revision": "21c9402aaa92a3c24cab",
+    "url": "/static/js/main.21c9402a.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5c8939a5f055ee05db267bea751943d0",
+    "url": "/index.html"
+  }
+];
